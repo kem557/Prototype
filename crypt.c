bf2f26413231ba7af8443955dd0a1960ba576690
CharlieHerman <charlieherman22@gmail.com>
2022-12-07 19:55:41 -0700

    Sanatized user inputs, added processing modes (CPU and GPU multithreading is TODO), optimized single-threaded encryption,  reformatted runtime console output.

--- crypt.c
+++ crypt.c
@@ -11,13 +11,15 @@
 #include "qdbmp/qdbmp.h"
 #include "b64/b64.h"
 #include <time.h>
+#include <sys/types.h>
+#include <sys/timeb.h>
 #pragma warning(disable : 4996)
 #pragma warning(disable : 6386)
 #pragma warning(disable : 6262)
 #pragma warning(disable : 4996)
 
 
-double* crypt(int kyber_implementation, char* input_file_path, char* encryption_file_path, char* decryption_file_path) {
+int crypt(int kyber_implementation, char* input_file_path, char* encryption_file_path, char* decryption_file_path, int mode) {
 	/*
 	Kyber Encryption/Decryption Demo
 	How to use:
@@ -62,28 +64,20 @@ double* crypt(int kyber_implementation, char* input_file_path, char* encryption_
 	uint8_t buffer32[32] = "";
 	uint8_t decrypted_message[32] = "";
 	uint8_t header[54] = "";
-	clock_t start_enc, end_enc;
+	int start_enc, end_enc;
+	int start_dec, end_dec;
 	long double difference_enc, difference_dec;
-	clock_t start_dec, end_dec;
 	long long fileSize;
 	long long i = 0;
 	int k = 0;
+	int j = 0;
 
-	/*
-
-	Keygen
-
-	*/
-	OQS_KEM_kyber_512_keypair(public_key_512, secret_key_512);
-	OQS_KEM_kyber_768_keypair(public_key_768, secret_key_768);
-	OQS_KEM_kyber_1024_keypair(public_key_1024, secret_key_1024);
 
 	/*
 	
-	Encryption
+	Read Input File
 
 	*/
-	start_enc = clock();
 	f_input = fopen(input_file_path, "rb");
 	if (!f_input) {
 		printf("Invalid file path, file might not exist: %s\nProgram End.\n", input_file_path);
@@ -93,82 +87,134 @@ double* crypt(int kyber_implementation, char* input_file_path, char* encryption_
 	fileSize = _ftelli64(f_input);
 	_fseeki64(f_input, 0, SEEK_SET);
 
-	fread(header,1,54,f_input);
-
-	f_encryption = fopen(encryption_file_path, "wb+");
-	fwrite(header,54,1,f_encryption);
-	i = 0;
-	while (i < fileSize - 54) {
-		for (k = 0; k < 32; k++) buffer32[k] = 0x0;
-		fread(buffer32, 1, 32, f_input);
-		if (kyber_implementation == 1) {
-			OQS_KEM_kyber_512_encrypt(ciphertext_512, buffer32, public_key_512);
-			fwrite(ciphertext_512, 1, OQS_KEM_kyber_512_length_ciphertext, f_encryption);
-		}
-		else if (kyber_implementation == 2) {
-			
-			OQS_KEM_kyber_768_encrypt(ciphertext_768, buffer32, public_key_768);
-			fwrite(ciphertext_768, 1, OQS_KEM_kyber_768_length_ciphertext, f_encryption);
-		}
-		else if (kyber_implementation == 3) {
-			OQS_KEM_kyber_1024_encrypt(ciphertext_1024, buffer32, public_key_1024);
-			fwrite(ciphertext_1024, 1, OQS_KEM_kyber_1024_length_ciphertext, f_encryption);
-		}
-		i += 32;
-	}
-	end_enc = clock();
-	fclose(f_input);
-	fclose(f_encryption);
 
 	/*
 	
-	Decryption 
+	Initialize cipherfile
 	
 	*/
-	start_dec = clock();
-	f_encryption = fopen(encryption_file_path, "rb");
-	if (!f_encryption) {
-		printf("Invalid file path, file might not exist: %s\nProgram End.\n", encryption_file_path);
-		return 0;
-	}
-	_fseeki64(f_encryption, 0, SEEK_END);
-	fileSize = _ftelli64(f_encryption);
-	_fseeki64(f_encryption, 0, SEEK_SET);
+	size_t cipherfile_512_size = 24 * fileSize;
+	size_t cipherfile_768_size = 34 * fileSize;
+	size_t cipherfile_1024_size = 49 * fileSize;
+	uint8_t *cipherfile_512 = (uint8_t*)calloc(1, cipherfile_512_size);
+	uint8_t *cipherfile_768 = (uint8_t*)calloc(1, cipherfile_768_size);
+	uint8_t *cipherfile_1024 = (uint8_t*)calloc(1, cipherfile_1024_size);
 
-	fread(header, 1, 54, f_encryption);
+	/*
 
-	f_decryption = fopen(decryption_file_path, "wb+");
-	fwrite(header, 54, 1, f_decryption);
-	i = 0;
-	while (i < fileSize - 54) {
-		for (k = 0; k < 32; k++) buffer32[k] = 0x0;
+	Keygen
+
+	*/
+	
+	OQS_KEM_kyber_512_keypair(public_key_512, secret_key_512);
+
+	OQS_KEM_kyber_768_keypair(public_key_768, secret_key_768);
+
+	OQS_KEM_kyber_1024_keypair(public_key_1024, secret_key_1024);
+
+	/*
+	
+	Encryption
+
+	*/
+	if (mode == 1) {
+		fread(header, 1, 54, f_input);
+		f_encryption = fopen(encryption_file_path, "wb+");
+		fwrite(header, 54, 1, f_encryption);
+		i = 0;
+		j = 0;
+		start_enc = clock();
+		while (i < fileSize - 54) {
+			fread(buffer32, 1, 32, f_input);
+			if (kyber_implementation == 1) {
+				OQS_KEM_kyber_512_encrypt((cipherfile_512+j), buffer32, public_key_512);
+				j += 768;
+			}
+			else if (kyber_implementation == 2) {
+				OQS_KEM_kyber_768_encrypt((cipherfile_768+j), buffer32, public_key_768);
+				j += 1088;
+			}
+			else if (kyber_implementation == 3) {
+				OQS_KEM_kyber_1024_encrypt((cipherfile_1024+j), buffer32, public_key_1024);
+				j += 1568;
+			}
+			i += 32;
+		}
+		end_enc = clock();
+		/*
+		
+		Write Encryption to File
+
+		*/
 		if (kyber_implementation == 1) {
-			for (k = 0; k < OQS_KEM_kyber_512_length_ciphertext; k++) ciphertext_512[k] = 0x0;
-			fread(ciphertext_512, 1, OQS_KEM_kyber_512_length_ciphertext, f_encryption);
-			OQS_KEM_kyber_512_decrypt(buffer32, ciphertext_512, secret_key_512);
-			
-			i += OQS_KEM_kyber_512_length_ciphertext;
+			fwrite(cipherfile_512, cipherfile_512_size, 1, f_encryption);
 		}
 		else if (kyber_implementation == 2) {
-			for (k = 0; k < OQS_KEM_kyber_768_length_ciphertext; k++) ciphertext_768[k] = 0x0;
-			fread(ciphertext_768, 1, OQS_KEM_kyber_768_length_ciphertext, f_encryption);
-			OQS_KEM_kyber_768_decrypt(buffer32, ciphertext_768, secret_key_768);
-			i += OQS_KEM_kyber_768_length_ciphertext;
+			fwrite(cipherfile_768, cipherfile_768_size, 1, f_encryption);
 		}
 		else if (kyber_implementation == 3) {
-			for (k = 0; k < OQS_KEM_kyber_1024_length_ciphertext; k++) ciphertext_1024[k] = 0x0;
-			fread(ciphertext_1024, 1, OQS_KEM_kyber_1024_length_ciphertext, f_encryption);
-			OQS_KEM_kyber_1024_decrypt(buffer32, ciphertext_1024, secret_key_1024);
-			i += OQS_KEM_kyber_1024_length_ciphertext;
+			fwrite(cipherfile_1024, cipherfile_1024_size, 1, f_encryption);
 		}
-		fwrite(buffer32, 1, 32, f_decryption);
+		fclose(f_input);
+		fclose(f_encryption);
+
+		/*
+
+		Decryption
+
+		*/
+		
+		f_encryption = fopen(encryption_file_path, "rb");
+		if (!f_encryption) {
+			printf("Invalid file path, file might not exist: %s\nProgram End.\n", encryption_file_path);
+			return 0;
+		}
+		_fseeki64(f_encryption, 0, SEEK_END);
+		fileSize = _ftelli64(f_encryption);
+		_fseeki64(f_encryption, 0, SEEK_SET);
+
+		fread(header, 1, 54, f_encryption);
+
+		f_decryption = fopen(decryption_file_path, "wb+");
+		fwrite(header, 54, 1, f_decryption);
+		i = 0;
+		start_dec = clock();
+		while (i < fileSize - 54) {
+			for (k = 0; k < 32; k++) buffer32[k] = 0x0;
+			if (kyber_implementation == 1) {
+				for (k = 0; k < OQS_KEM_kyber_512_length_ciphertext; k++) ciphertext_512[k] = 0x0;
+				fread(ciphertext_512, 1, OQS_KEM_kyber_512_length_ciphertext, f_encryption);
+				OQS_KEM_kyber_512_decrypt(buffer32, ciphertext_512, secret_key_512);
+
+				i += OQS_KEM_kyber_512_length_ciphertext;
+			}
+			else if (kyber_implementation == 2) {
+				for (k = 0; k < OQS_KEM_kyber_768_length_ciphertext; k++) ciphertext_768[k] = 0x0;
+				fread(ciphertext_768, 1, OQS_KEM_kyber_768_length_ciphertext, f_encryption);
+				OQS_KEM_kyber_768_decrypt(buffer32, ciphertext_768, secret_key_768);
+				i += OQS_KEM_kyber_768_length_ciphertext;
+			}
+			else if (kyber_implementation == 3) {
+				for (k = 0; k < OQS_KEM_kyber_1024_length_ciphertext; k++) ciphertext_1024[k] = 0x0;
+				fread(ciphertext_1024, 1, OQS_KEM_kyber_1024_length_ciphertext, f_encryption);
+				OQS_KEM_kyber_1024_decrypt(buffer32, ciphertext_1024, secret_key_1024);
+				i += OQS_KEM_kyber_1024_length_ciphertext;
+			}
+			fwrite(buffer32, 1, 32, f_decryption);
+		}
+		end_dec = clock();
+		difference_enc = (long double)(end_enc - start_enc) / CLOCKS_PER_SEC * 1000;
+		difference_dec = (long double)(end_dec - start_dec) / CLOCKS_PER_SEC * 1000;
+		fclose(f_encryption);
+		fclose(f_decryption);
+		printf("\nTime to encrypt file %s (ms): %5.0Lf", input_file_path, difference_enc);
+		printf("\nTime to decrypt file %s (ms): %5.0Lf", input_file_path, difference_dec);
 	}
-	end_dec = clock();
-	difference_enc = (long double) (end_enc - start_enc) / (CLOCKS_PER_SEC);
-	difference_dec =  (long double) (end_dec - start_dec)/ (CLOCKS_PER_SEC);
-	fclose(f_encryption);
-	fclose(f_decryption);
-	printf("Time to encrypt file %s (s): %Lf\n", input_file_path, difference_enc);
-	printf("Time to decrypt file %s (s): %Lf\n", input_file_path, difference_dec);
+	else if (mode == 2) {
+		printf("\nMulti-Threaded (CPU) Encryption/Decryption not available yet");
+	}
+	else if (mode == 3) {
+		printf("\nMulti-Threaded (GPU) Encryption/Decryption not available yet");
+	}
 	return 0;
 }
--- main.cpp
+++ main.cpp
@@ -14,8 +14,10 @@ const string CASCADE_CLASSIFIER = "Resources/haarcascade_frontalface_default.xml
 // function declarations
 string getInputFile();
 int getKyber();
-int getFaces(string, int);
-double* crypt(int kyber_implementation, char* input_file_path, char* encryption_file_path, char* decryption_file_path);
+int getMode();
+int getFaces(string, int, int);
+int crypt(int kyber_implementation, char* input_file_path, char* encryption_file_path, char* decryption_file_path, int mode);
+inline bool exists_test(const string& name);
 
 // Main
 int main() 
@@ -23,10 +25,11 @@ int main()
 	// Step 1: get input file from user
 	string path = getInputFile();
 	int kyberImplementation = getKyber();
+	int mode = getMode();
 	Mat img = imread(path);
 
 	// Step 2: Facial Recognition: generate face image files, duplicate input image with face rectangles drawn
-	getFaces(path, kyberImplementation);
+	getFaces(path, kyberImplementation, mode);
 
 	// TODO Step 3: encrypt and decrypt each face file
 
@@ -38,25 +41,60 @@ int main()
 // TODO:  add input validation
 string getInputFile()
 {
+	int condition = 0;
 	string path;
 	cout << "Enter input file, including extension (from Input folder):\n\tEnter:\t";
-	cin >> path;
-	path = "Input/" + path;
+	while (!condition) {
+		cin >> path;
+		path = "Input/" + path;
+		if (exists_test(path)) {
+			condition = 1;
+		}
+		else {
+			printf("\nInvalid File Path\n\tEnter:\t");
+		}
+	}
 	return path;
 }
 
 int getKyber()
 {
+	int condition = 0;
 	int kyberImplementation;
 	cout << "\nEnter Kyber Implementation: 1. Kyber512\t2. Kyber768\t3. Kyber1024\n\tEnter:\t";
-	cin >> kyberImplementation;
-	
+	while (!condition) {
+		cin >> kyberImplementation;
+		if (kyberImplementation >= 1 && kyberImplementation <= 3) {
+			condition = 1;
+		}
+		else {
+			printf("\nInvalid Input (1-3)\n\tEnter:\t");
+		}
+	}
 	return kyberImplementation;
+}
+
+int getMode()
+{
+	int condition = 0;
+	int mode;
+	cout << "\nEnter Processing Mode: 1. Single Threaded\t2. Multi-Threaded (CPU)\t3. Multi-Threaded (GPU)\n\tEnter:\t";
+	while (!condition) {
+		cin >> mode;
+		if (mode >= 1 && mode <= 3) {
+			condition = 1;
+		}
+		else {
+			printf("\nInvalid Input (1-3)\n\tEnter:\t");
+		}
+	}
+
+	return mode;
 }
 
 
 // Step 2: detect faces, output face files and face outlines file
-int getFaces(string inputPath, int kyberImplementation)
+int getFaces(string inputPath, int kyberImplementation, int mode)
 {
 	// Load facial recognition XML
 	CascadeClassifier faceCascade;
@@ -104,15 +142,17 @@ int getFaces(string inputPath, int kyberImplementation)
 		strncpy(decrypted_path, decryptedPath.c_str(), sizeof(decrypted_path));
 
 
-		crypt(kyberImplementation,output_path,encrypted_path,decrypted_path);
-		Mat encrypted = imread(encrypted_path);
-		Mat decrypted = imread(decrypted_path);
-		string encrypted_window = "Encrypted Image " + to_string(i);
-		string decrypted_window = "Decrypted Image " + to_string(i);
-		namedWindow(encrypted_window, WINDOW_NORMAL);
-		namedWindow(decrypted_window, WINDOW_NORMAL);
-		imshow(encrypted_window, encrypted);
-		imshow(decrypted_window, decrypted);
+		crypt(kyberImplementation,output_path,encrypted_path,decrypted_path, mode);
+		if (exists_test(encrypted_path) && exists_test(decrypted_path)) {
+			Mat encrypted = imread(encrypted_path);
+			Mat decrypted = imread(decrypted_path);
+			string encrypted_window = "Encrypted Image " + to_string(i);
+			string decrypted_window = "Decrypted Image " + to_string(i);
+			namedWindow(encrypted_window, WINDOW_NORMAL);
+			namedWindow(decrypted_window, WINDOW_NORMAL);
+			imshow(encrypted_window, encrypted);
+			imshow(decrypted_window, decrypted);
+		}
 	}
 
 	// output full image with overlayed rectangles
@@ -123,4 +163,14 @@ int getFaces(string inputPath, int kyberImplementation)
 	waitKey(0);
 
 	return 0;
+}
+
+inline bool exists_test(const string& name) {
+	if (FILE* file = fopen(name.c_str(), "r")) {
+		fclose(file);
+		return true;
+	}
+	else {
+		return false;
+	}
 }
